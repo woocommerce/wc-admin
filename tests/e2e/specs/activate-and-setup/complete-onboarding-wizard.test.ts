fc7833ec2b526b75be7f3ee8a66f2fbb8e3d26cc
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2021-05-25 21:09:02 +0800

    Fix missing semi-colon

--- tests/e2e/specs/activate-and-setup/complete-onboarding-wizard.test.ts
+++ tests/e2e/specs/activate-and-setup/complete-onboarding-wizard.test.ts
@@ -24,7 +24,7 @@ describe( 'Store owner can complete onboarding wizard', () => {
 	} );
 
 	it( 'can start the profile wizard', async () => {
-		await resetWooCommerceState()
+		await resetWooCommerceState();
 
 		await profileWizard.navigate();
 	} );
